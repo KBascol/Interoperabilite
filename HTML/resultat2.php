43821490162abd8a6da4530ec73830f0e99ca751
bouacheria bachir <bouacheria.bachir@hotmail.fr>
2015-03-08 17:09:10 +0100

    ajout du formulaire dans la page ciné

--- HTML/resultat2.php
+++ HTML/resultat2.php
@@ -38,18 +38,6 @@
   ?>
   <!--/row-->
 
-  <div class="col-lg-12 text-center v-center">
-    <div class="col-lg-6 col-lg-offset-3 v-center" style="background-color:#2c3e50; margin-top:100px; padding-bottom:30px; border-radius:10px;">
-      <h3 style="color:#fff;">Quel est le nom du lieu ?</h3>
-      <br>
-      <form class="col-lg-12" action="#" method="">
-        <div class="input-group col-lg-10" style="text-align:center;margin:0 auto;">
-          <input class="form-control input-lg" name="" placeholder="Nom du lieu" type="text">
-          <span class="input-group-btn"><input class="btn btn-lg btn-primary" value="Ajouter le lieu" type="submit" style="background-color:#18bc9c;border-color:#18bc9c"></span>
-        </div>
-      </form>
-    </div>
-  </div>
 
 </section>   
 
