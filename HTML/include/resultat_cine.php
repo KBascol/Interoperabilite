e2f6de1ada858ccf4c6ad551b154a6cde944c4e1
Kevin BASCOL <bascol.kevin@hotmail.fr>
2015-03-08 20:33:05 +0100

    Remise parsage lien google cine

--- HTML/include/resultat_cine.php
+++ HTML/include/resultat_cine.php
@@ -1,19 +1,32 @@
 <?php
-	
-	$pageFilmAffiche = file_get_contents("http://www.allocine.fr/seance/salle_gen_csalle=P0231.html");
-	
-	preg_match_all("/<\!--\s*picturezone\s*-->\n<div[^*<\/]*<\/div>\n<[^>]*>\n<[^>]*>\n<h2>\n<[^>]*>([^<]*)<\/a>/", $pageFilmAffiche, $titre,PREG_SET_ORDER);
-	preg_match_all("/<\!--\s*\/notationbar\s*-->\n<p>\n([^<]*)<\/p>/", $pageFilmAffiche, $description, PREG_SET_ORDER);
-	preg_match_all("/<\!--\s*class=\"titlebar\"\s*-->\n<p>\n(.*)\n\(([^\)]*)\)\n[^<]*<b>([^<]*)<\/b>/",$pageFilmAffiche, $information, PREG_SET_ORDER);
-	preg_match_all("/<p>\nDe\s<a\shref=\'\/personne\/fichepersonne_gen_cpersonne=[^>]*>([^<]*)<\/a>/",$pageFilmAffiche, $realisateur, PREG_SET_ORDER);
-	preg_match_all("/<a\shref=\"\/film\/fichefilm_gen_cfilm=[^>]*><img\ssrc=\'([^\']*)\'[^>]*><\/a>/", $pageFilmAffiche, $image, PREG_SET_ORDER);
+$ch = curl_init();
 
-	preg_match_all("/<div\sclass=\"showtimescore js_pane_wrapper\"><div\sclass=\"pane\" rel=\"0\">\n<p>\n<span[^<]*<\/span>,\n<span[^<]*<\/span>\n<\/p>\n<div[^<]*<ul>(.*)<\/ul>/", $pageFilmAffiche, $ligneSeance, PREG_SET_ORDER);
+// Définition de l'URL et autres options appropriées
+curl_setopt($ch, CURLOPT_URL, "http://www.google.fr/search?q=allocine+".preg_replace("/\_/","+",$_GET['adr']));
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	
-	$size = sizeof($titre);
+// Récupération de l'URL et passage au navigateur
+$ret=curl_exec($ch);
+// Fermeture de la ressource cURL et libération des ressources systèmes
+curl_close($ch);
+preg_match("#http://www.allocine\.fr/seance/[^&]*#",$ret,$res);
+    
+if(isset($res[0])){
 
-	for($i = 0; $i< $size; $i++) {
+    $pageFilmAffiche = file_get_contents($res[0]);
+
+    preg_match_all("/<\!--\s*picturezone\s*-->\n<div[^*<\/]*<\/div>\n<[^>]*>\n<[^>]*>\n<h2>\n<[^>]*>([^<]*)<\/a>/", $pageFilmAffiche, $titre,PREG_SET_ORDER);
+    preg_match_all("/<\!--\s*\/notationbar\s*-->\n<p>\n([^<]*)<\/p>/", $pageFilmAffiche, $description, PREG_SET_ORDER);
+    preg_match_all("/<\!--\s*class=\"titlebar\"\s*-->\n<p>\n(.*)\n\(([^\)]*)\)\n[^<]*<b>([^<]*)<\/b>/",$pageFilmAffiche, $information, PREG_SET_ORDER);
+    preg_match_all("/<p>\nDe\s<a\shref=\'\/personne\/fichepersonne_gen_cpersonne=[^>]*>([^<]*)<\/a>/",$pageFilmAffiche, $realisateur, PREG_SET_ORDER);
+    preg_match_all("/<a\shref=\"\/film\/fichefilm_gen_cfilm=[^>]*><img\ssrc=\'([^\']*)\'[^>]*><\/a>/", $pageFilmAffiche, $image, PREG_SET_ORDER);
+
+    preg_match_all("/<div\sclass=\"showtimescore js_pane_wrapper\"><div\sclass=\"pane\" rel=\"0\">\n<p>\n<span[^<]*<\/span>,\n<span[^<]*<\/span>\n<\/p>\n<div[^<]*<ul>(.*)<\/ul>/", $pageFilmAffiche, $ligneSeance, PREG_SET_ORDER);
+
+    $size = sizeof($titre);
+
+    for($i = 0; $i< $size; $i++) {
 ?>
     <!-- un row qui contient les information d'un livre -->
     <div class="row">
@@ -29,33 +42,43 @@
           <b>Date de sorti : </b><span><?php echo $information[$i][3]; ?></span><br>
           <b>Description : </b><span><?php if(isset($description[$i][1])){ echo $description[$i][1]; } else { echo "Pas de description disponible"; } ?></span><br>
           <b>Seances du <?php echo date('d/m/Y'); ?> : </b>
-          	<span>
-          		<?php 
-          			if(isset($ligneSeance[$i][0])) {
-						preg_match_all("/<em[^>]*>(\d{2}\:\d{2})<\/em>/", $ligneSeance[$i][0], $seances,PREG_SET_ORDER);
-	          			$sizeSeances=sizeof($seances);
-	          			if($sizeSeances==0) {
-	          				echo 'Prochainement';
-	          			} else {
-	          				for($j=0; $j < $sizeSeances; $j++) {
-								echo $seances[$j][1];
-								if($j < $sizeSeances-1) {
-									echo ', ';
-								} else {
-									echo '.';
-								}
-							}	
-	          			}
-          			}else {
-          				echo 'Prochainement';
-          			}
-          			
-				?>
-          	</span><br>
+              <span>
+                  <?php 
+                      if(isset($ligneSeance[$i][0])) {
+                        preg_match_all("/<em[^>]*>(\d{2}\:\d{2})<\/em>/", $ligneSeance[$i][0], $seances,PREG_SET_ORDER);
+                          $sizeSeances=sizeof($seances);
+                          if($sizeSeances==0) {
+                              echo 'Prochainement';
+                          } else {
+                              for($j=0; $j < $sizeSeances; $j++) {
+                                echo $seances[$j][1];
+                                if($j < $sizeSeances-1) {
+                                    echo ', ';
+                                } else {
+                                    echo '.';
+                                }
+                            }    
+                          }
+                      }else {
+                          echo 'Prochainement';
+                      }
+                      
+                ?>
+              </span><br>
         </div>
       </div>
     </div>
     <!--/row-->
     
     <br><br>
-<?php } ?>
+<?php 
+    }
+}
+else {
+?>
+    <div class="row">
+        <h4 class="media-heading">Séances introuvables.<h4>
+    </div>
+<?php
+}
+?>
