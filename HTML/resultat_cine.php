1e3ded64d5b348bb421a0e0fc45e560b371eb12c
kevL974 <kev.laoussing@gmail.com>
2015-03-08 16:36:01 +0100

    ajout de la fonctionnalité recherche horaire non fontionnel

--- HTML/resultat_cine.php
+++ HTML/resultat_cine.php
@@ -9,6 +9,11 @@
 		preg_match_all("/<\!--\s*class=\"titlebar\"\s*-->\n<p>\n(.*)\n\(([^\)]*)\)\n[^<]*<b>([^<]*)<\/b>/",$pageFilmAffiche, $information, PREG_SET_ORDER);
 		preg_match_all("/<p>\nDe\s<a\shref=\'\/personne\/fichepersonne_gen_cpersonne=[^>]*>([^<]*)<\/a>/",$pageFilmAffiche, $realisateur, PREG_SET_ORDER);
 		preg_match_all("/<a\shref=\"\/film\/fichefilm_gen_cfilm=[^>]*><img\ssrc=\'([^\']*)\'[^>]*><\/a>/", $pageFilmAffiche, $image, PREG_SET_ORDER);
+
+		preg_match_all("/<div\sclass=\"showtimescore js_pane_wrapper\"><div\sclass=\"pane\" rel=\"0\">\n<p>\n<span[^<]*<\/span>,\n<span[^<]*<\/span>\n<\/p>\n<div\s[^<]*<ul>((?:<li><em[^>]*>\d{2}\:\d{2}<\/em><\/li>)+<li\sclass=\"last\"><em[^>]*>\d{2}\:\d{2}<\/em><\/li>)/", $pageFilmAffiche, $ligneSeance, PREG_SET_ORDER);
+
+		echo $ligneSeance[0];
+		preg_match_all("/>(\d{2}:\d{2})</", $ligneSeance[0], $seances,PREG_SET_ORDER);
 		$size = sizeof($titre);
 	
 		echo '[';
@@ -21,8 +26,15 @@
 					',"réalisateur" : '.json_encode($realisateur[$i][1]).
 					',"genre" : '.json_encode($information[$i][2]).
 				 	',"description" : '.json_encode($description[$i][1]).
-				 	',"image" : '.json_encode($image[$i][1]).
-					'}';
+				 	',"image" : '.json_encode($image[$i][1]);
+
+			$sizeSeances=sizeof($seances);
+			echo ', "seances" : ';
+			for($j=1;$j<sizeSeances;$j++) {
+				echo json_encode($seances[$i][$j]) .',';
+			}
+				 	
+			echo 	'}';
 			
 			$i++;
 			if($i < $size){
