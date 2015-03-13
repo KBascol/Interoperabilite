d68ceb0e73eff7b52244508f8409431c736b4bbb
Kevin BASCOL <bascol.kevin@hotmail.fr>
2015-03-13 13:16:21 +0100

    virage resultat resto

--- HTML/include/resultat_resto.php
+++ HTML/include/resultat_resto.php
@@ -1,23 +1,9 @@
             <!-- un row qui contient les information d'un ciné -->
             <div class="row" style="margin-top: 15px;">
               <div class="media col-lg-10 col-lg-offset-1">
-                <div class="media-left">
-                    <img class="media-object" src="img/50n.jpg" alt="..." style="height: 237px; width:160px;">
-                </div>
                 <div class="media-body" style="font-size:13px;">
-                  <h4 class="media-heading">Cinquante Nuances de Grey</h4>
-                  <span>L'histoire d'une romance passionnelle, et sexuelle, entre un jeune homme riche amateur de femmes, et une étudiante vierge de 22 ans.</span><br>
-                  <b>Date de sortie : </b><span>11 février 2015 (2h5min)</span><br>
-                  <b>Réalisé par : </b><span>Sam Taylor-Johnson</span><br>
-                  <b>Genre : </b><span>Erotique , Drame , Romance</span><br>
-                  <b>Nationalité : </b><span>Américain</span><br>
-                  <b>Horaires et séances : </b><br>
+                  <h4 class="media-heading">Menu:</h4>
                   <ul>
-                    <li><b>Vendredi : </b> <span class="label label-success">13:50</span> <span class="label label-success">16:30</span> <span class="label label-success">19:10</span> <span class="label label-success">21:45</span> </li>
-                    <li><b>Samedi : </b> <span class="label label-success">13:50</span> <span class="label label-success">16:30</span> <span class="label label-success">19:10</span> <span class="label label-success">21:45</span> </li>
-                    <li><b>Dimanche : </b> <span class="label label-success">13:50</span> <span class="label label-success">16:30</span> <span class="label label-success">19:10</span> <span class="label label-success">21:45</span></li>
-                    <li><b>Lundi : </b> <span class="label label-success">13:50</span> <span class="label label-success">16:30</span> <span class="label label-success">19:10</span> <span class="label label-success">21:45</span> </li>
-                    <li><b>Mardi : </b> <span class="label label-success">13:50</span> <span class="label label-success">16:30</span> <span class="label label-success">19:10</span> <span class="label label-success">21:45</span> </li>
                   </ul>
                 </div>
               </div>
